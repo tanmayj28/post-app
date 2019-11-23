7b3c42bed61f74f093d8fabbb5cc5c62891c4753
7anminator <tanmay@happyness.net>
2019-11-23 18:07:06 +0530

    adds increment and destroy upvote

--- app/models/upvote.rb
+++ app/models/upvote.rb
@@ -4,4 +4,17 @@ class Upvote < ApplicationRecord
 
   validates :post, uniqueness: { scope: :visitor }
   validates :visitor, uniqueness: { scope: :post }
+
+  after_create :increment_upvote
+  after_destroy :destroy_upvote
+
+  def increment_upvote
+    post.upvotes_count += 1
+    post.save
+  end
+
+  def destroy_upvote
+    post.upvotes_count -= 1
+    post.save
+  end
 end
